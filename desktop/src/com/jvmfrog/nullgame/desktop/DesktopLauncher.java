5d9e484457896a451be7b0a68f75924a3a8249cb
Kirill <kirbo06@mail.ru>
2022-05-12 18:04:29 +0200

    logo update

--- desktop/src/com/jvmfrog/nullgame/desktop/DesktopLauncher.java
+++ desktop/src/com/jvmfrog/nullgame/desktop/DesktopLauncher.java
@@ -1,12 +1,17 @@
 package com.jvmfrog.nullgame.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.jvmfrog.nullgame.Main;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Main(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+        config.title = "null";
+        config.addIcon("logo.png", Files.FileType.Internal);
+
+        new LwjglApplication(new Main(), config);
+    }
 }
