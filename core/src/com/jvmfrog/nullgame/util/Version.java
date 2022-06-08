00b218765648db91cbdf978af618b1438516e194
Kirill <kirbo06@mail.ru>
2022-06-08 17:52:08 +0200

    added Version.java to .gitignore

--- core/src/com/jvmfrog/nullgame/util/Version.java
+++
@@ -1,9 +0,0 @@
-
-package com.jvmfrog.nullgame.util;
-
-public class Version {
-    public static final String VERSION = "0.1";
-    public static final String CODE_VERSION = "1";
-    public static final long BUILD_TIME = 1652885918581L;
-    public static final String BUILD_TIME_FORMATED = "18-05-2022 17:58:38";
-}
